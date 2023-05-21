829adb2f42da49082cd288546509ed5757a99f9f
rafael da silva gomes <rafaelsilvagomesreal@hotmail.com>
2023-05-21 12:35:16 -0300

    atualização nos arquivo json brainAI e testeAi.js

--- testeAi.js
+++ testeAi.js
@@ -1,29 +1,40 @@
 const { resolve } = require('path');
 const readline = require('readline');
-const brain = require('./brainAI');
+const brain = require('./brainAI.json');
 
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
-
-
+// console.log(brain);
 
 class AIPessoal {
     constructor(name){
         this.name = name;
     }
-
+    // função inicial de boas vindas
     async boasVindas(duration)
     {
         await new Promise(resolve => setTimeout(resolve, duration));
     }
+    // função que iniciar a buscar da palavra chave
     async suaPesquisa(duration){
         await new Promise(resolve => setTimeout(resolve, duration))
-        rl.question(`\t O que você gostari de fazer? `, (input_pesquisa) => {
-            console.log(`\t\n Iniciando... Buscar ${input_pesquisa}`);
+        rl.question(`\t Entre com a Palavra Chave? `, (input_pesquisa) => {
+            do {
+                if(input_pesquisa == "programador"){
+                    console.log(brain.Programador);
+                }else if(input_pesquisa == "matador"){
+                    console.table(brain.matador);
+                }else if(input_pesquisa == "cantora")
+                {
+                    console.table(brain.cantora);
+                }else{
+                    console.log(`\tInformações errado porfavo tente novamento!...\n`)
+                    myAI.suaPesquisa(1000);
+                }
+            } while (input_pesquisa == "sair");
         })
     }
 }
@@ -38,10 +49,11 @@ class AI extends AIPessoal{
         rl.question(`\tOla como se chamar? `, (input_name) => {
             console.log(`\tOla ${input_name} tudo bem?`)
             console.log(`\tO que gostaria de fazer? ${input_name}`);
-            rl.on("line", (input_perguntas) => {
-                console.log(`\t\nSua persquisa.... \n${input_perguntas}`)
-                if (input_perguntas == "sair") {
-                    rl.close()
+            console.log(`\tInformações de sua busca...`);
+            rl.on("line", (input_pesquisa) => {
+                console.log(`\t\nSua persquisa.... \n${input_pesquisa}`);
+                if(input_pesquisa == "sair"){
+                    rl.close();
                 }else{
                     myAI.suaPesquisa(2000);
                 }
@@ -59,6 +71,6 @@ class AI extends AIPessoal{
 }
 
 const myAI = new AI("Rafael");
-myAI.boasVindas(1000).then( () => {
+myAI.boasVindas(2000).then( () => {
     myAI.falar();
 } )
